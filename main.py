ec69e8b944c5848072f06c1658a672ce7f5b4267
JoeyD <joseph.derouin23@ycdsbk12.ca>
2021-03-31 13:31:12 -0400

    Test

--- main.py
+++ main.py
@@ -1,51 +1,251 @@
-""" 
-A basic pygame template
-"""
- 
-import pygame
- 
-# Define some colors
-BLACK    = (   0,   0,   0)
-WHITE    = ( 255, 255, 255)
-GREEN    = (   0, 255,   0)
-RED      = ( 255,   0,   0)
- 
-pygame.init()
-  
-# Set the width and height of the screen [width, height]
-size = (700, 500)
-screen = pygame.display.set_mode(size)
- 
-pygame.display.set_caption("My Game")
- 
-#Loop until the user clicks the close button.
-done = False
- 
-# Used to manage how fast the screen updates
-clock = pygame.time.Clock()
- 
-# -------- Main Program Loop -----------
-while not done:
-    # --- Main event loop
-    for event in pygame.event.get(): # User did something
-        if event.type == pygame.QUIT: # If user clicked close
-            done = True # Flag that we are done so we exit this loop
-  
-    # --- Game logic should go here
- 
-    # --- Drawing code should go here
-     
-    # First, clear the screen to white or whatever background colour. 
-    # Don't put other drawing commands above this, or they will be erased with this command.
-    screen.fill(WHITE)
-     
-    # --- Go ahead and update the screen with what we've drawn.
-    pygame.display.flip()
- 
-    # --- Limit to 60 frames per second
-    clock.tick(60)
-     
-# Close the window and quit.
-# If you forget this line, the program will 'hang'
-# on exit if running from IDLE.
-pygame.quit()
+###Title###
+print(" ")
+print(" ")
+print("-------------------------- Multiple Choice Math and Computers Quiz -------------------------- ")
+print(" ")
+print(" ")
+
+###Rules###
+print("-------No Calculators------")
+print(" ")
+print("The first 15 questions are a simple multiple math test. Read the problem and choose A, B, C, or D. At the end it will tell you your score and %. After the math quiz, you will do a 10 question Computer Quiz. Good luck!")
+print(" ")
+
+
+###Question 1###
+d1a = input ("What is 1 + 1?: A) 2 B) 4 [A/B]? : ")
+if d1a == "A": 
+    print ("That is correct!")
+elif d1a == "B":
+    print ("That is incorrect.")
+print("Next Question")
+print(" ")
+
+###Question 2###
+d1a = input ("What is 4 x 4?: A) 16 B) 44 [A/B]? : ")
+if d1a == "A": 
+    print ("That is incorrect.")
+elif d1a == "B":
+    print ("That is correct!")
+print("Next Question")
+print(" ")
+
+###Question 3###
+d1a = input ("What is 10 x 100?: A) 10000 B) 1000 [A/B]? : ")
+if d1a == "A": 
+    print ("That is incorrect.")
+elif d1a == "B":
+    print ("That is correct!")
+print("Next Question")
+print(" ")
+
+###Question 4###
+d1a = input ("What is 16 - 40?: A) 24 B) -24 [A/B]? : ")
+if d1a == "A": 
+    print ("That is incorrect.")
+elif d1a == "B":
+    print ("That is correct!")
+print("Next Question")
+print(" ")
+
+###Question 5###
+d1a = input ("What is 16 x 8?: A) 128 B) 130 C) 124 D) 120 [A/B/C/D]? : ")
+if d1a == "A": 
+    print ("That is incorrect.")
+elif d1a == "B":
+    print ("That is correct!")
+elif d1a == "C":
+    print ("That is incorrect.")
+elif d1a == "D":
+    print ("That is incorrect.")
+print("Next Question")
+print(" ")
+
+###Question 6###
+d1a = input ("What is 80 x 5?: A) 136 B) 85 C) 456 D) 400 [A/B/C/D]? : ")
+if d1a == "A": 
+    print ("That is incorrect.")
+elif d1a == "B":
+    print ("That is incorrect.")
+elif d1a == "C":
+    print ("That is incorrect.")
+elif d1a == "D":
+    print ("That is correct!")
+print("Next Question")
+print(" ")
+
+###Question 7###
+d1a = input ("What is 90 - 3 x 4?: A) 80 B) 56 C) 78 D) 87 [A/B/C/D]? : ")
+if d1a == "A": 
+    print ("That is incorrect.")
+elif d1a == "B":
+    print ("That is incorrect.")
+elif d1a == "C":
+    print ("That is correct!")
+elif d1a == "D":
+    print ("That is incorrect.")
+print("Next Question")
+print(" ")
+
+###Question 8###
+d1a = input ("What is 100 x 5 - 6 / 2?: A) 497 B) 500 C) 456 D) 496 [A/B/C/D]? : ")
+if d1a == "A": 
+    print ("That is correct!")
+elif d1a == "B":
+    print ("That is incorrect.")
+elif d1a == "C":
+    print ("That is incorrect.")
+elif d1a == "D":
+    print ("That is incorrect.")
+print("Next Question")
+print(" ")
+
+###Question 9###
+d1a = input ("What is 89 - (6 x 4)?: A) 70 B) 56 C) 65 D) 76 [A/B/C/D]? : ")
+if d1a == "A": 
+    print ("That is incorrect.")
+elif d1a == "B":
+    print ("That is incorrect.")
+elif d1a == "C":
+    print ("That is correct!")
+elif d1a == "D":
+    print ("That is incorrect.")
+print("Next Question")
+print(" ")
+
+###Question 10###
+d1a = input ("What is 150 - (7 x 6) + 36? : A) 150 B) 129 C) 140 D) 144 [A/B/C/D]? : ")
+if d1a == "A": 
+    print ("That is incorrect.")
+elif d1a == "B":
+    print ("That is incorrect.")
+elif d1a == "C":
+    print ("That is incorrect.")
+elif d1a == "D":
+    print ("That is correct!")
+print("Next Question")
+print(" ")
+
+###Next Stage###
+print(" ")
+print("Algebra")
+print(" ")
+
+###Question 11###
+d1a = input ("X = 4, What is 4X?: A) 4 B) 8 C) 16 D) 44 [A/B/C/D]? : ")
+if d1a == "A": 
+    print ("That is incorrect.")
+elif d1a == "B":
+    print ("That is incorrect.")
+elif d1a == "C":
+    print ("That is correct!")
+elif d1a == "D":
+    print ("That is incorrect.")
+print("Next Question")
+print(" ")
+
+###Question 11###
+d1a = input ("X = 5, What is 6X + 16?: A) 30 B) 52 C) 36 D) 64 [A/B/C/D]? : ")
+if d1a == "A": 
+    print ("That is incorrect.")
+elif d1a == "B":
+    print ("That is correct!")
+elif d1a == "C":
+    print ("That is incorrect.")
+elif d1a == "D":
+    print ("That is incorrect.")
+print("Next Question")
+print(" ")
+
+###Question 12###
+d1a = input ("X = 2, What is 7X + (7 x 4 - 3) - 6?: A) 33 B) 36 C) 12 D) 49 [A/B/C/D]? : ")
+if d1a == "A": 
+    print ("That is correct!")
+elif d1a == "B":
+    print ("That is incorrect.")
+elif d1a == "C":
+    print ("That is incorrect.")
+elif d1a == "D":
+    print ("That is incorrect.")
+print("Next Question")
+print(" ")
+
+###Question 13###
+d1a = input ("X = 1, What is 5X x (7 + 4 - 2)2 ?: A) 14 B) 12 C) 16 D) 18 [A/B/C/D]? : ")
+if d1a == "A": 
+    print ("That is incorrect.")
+elif d1a == "B":
+    print ("That is incorrect.")
+elif d1a == "C":
+    print ("That is incorrect.")
+elif d1a == "D":
+    print ("That is correct!")
+print("Next Question")
+print(" ")
+
+###Question 14###
+d1a = input ("X = 9, What is 3X + ((5 + 6 - 3)2 - 6) ?: A) 37 B) 89 C) 66 D) 34 [A/B/C/D]? : ")
+if d1a == "A": 
+    print ("That is correct!")
+elif d1a == "B":
+    print ("That is incorrect.")
+elif d1a == "C":
+    print ("That is incorrect.")
+elif d1a == "D":
+    print ("That is incorrect.")
+print("Next Question")
+print(" ")
+
+###Question 15###
+d1a = input ("X = 4, What is 6X + (9 x 7) ?: A) 87 B) 81 C) 86 D) 89 [A/B/C/D]? : ")
+if d1a == "A": 
+    print ("That is correct!")
+elif d1a == "B":
+    print ("That is incorrect.")
+elif d1a == "C":
+    print ("That is incorrect.")
+elif d1a == "D":
+    print ("That is incorrect.")
+print("Next Question")
+print(" ")
+
+
+###Computer Quiz###
+print(" ")
+print(" ")
+print(" ")
+print("---------------Computer Quiz Time-----------------")
+print(" ")
+print(" ")
+print(" ")
+
+
+###Questions###
+question_prompts = [
+    "what color are apples?\n(a) Red/Greem\n(b) Purple\n(c) Orange\n\n",
+    "What color are bananas?\n(a) Teal\n(b) Magenta\n(c) Yellow\n\n",
+    "What color are strawberries?\n(a) Yellow\n(b) Red\n(c) Blue\n\n"
+]
+
+
+questions = [
+    Question(question_prompts[0], "A"),
+    Question(question_prompts[1], "B"),
+    Question(question_prompts[2], "B"),
+]
+
+###Adding up correct Answers###
+def run_test(questions):
+    score = 0
+    for question in questions:
+        answer = input(question.prompt)
+        if answer == question.answer:
+            score += 1
+    print("You got " + str(score) + "/" + str(len(questions)) + correct)
+
+run_test(question)
+
+class question:
+    def __init__(self, prompt, answer):
+        self.prompt = prompt
+        self.answer = answer
